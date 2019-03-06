fdb9d4ce3fd99ba1d841f1c056b08b542ce96f57
Benjamin Viramontes <benja120599@gmail.com>
2019-03-06 09:38:03 -0600

    Se añadió el método generarDatos que llena un arreglo con valores aleatorios "SI" o "NO"

--- src/Concurrencia.java
+++ src/Concurrencia.java
@@ -1,9 +1,36 @@
 
+import javax.swing.*;
+import java.awt.*;
+
+class VentanaPrincipal extends JFrame {
+	
+	public VentanaPrincipal() {
+		
+	}
+	
+	public String[] generarDatos() {
+		
+		int numero = 0; 
+		String[] arreglo = new String[100];
+		
+		for(int i=0; i < 100; i++) {
+			numero = (int)(Math.random() * 100) + 1;
+			if( (numero % 2) == 0) {
+				arreglo[i] = "SI";
+			} else if((numero % 2) != 0) {
+				arreglo[i] = "NO";
+			}
+		}
+		return arreglo;
+	}
+	
+}
+
+
 public class Concurrencia {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
